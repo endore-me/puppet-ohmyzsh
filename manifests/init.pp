daf65075aa3ce3f4b9a95a0ef8134005a8bbb23e
Jens Fahnenbruck <jigfox@me.com>
2013-05-08 09:38:38 +0200

    make sure directory is present

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class ohmyzsh {
   package { 'zsh': }
   include git
+  file { '/usr/src': ensure => directory }
   git::repo { 'oh-my-zsh':
     path   => '/usr/src/oh-my-zsh',
     source => 'git://github.com/robbyrussell/oh-my-zsh.git',
